d97c6e9e5d7081cec368cfa11f4fac652c4b7a43
kbalad <kirillbalad@gmail.com>
2020-06-13 15:11:54 +0300

    add radix sort

--- src/algorithm/sorting/linear/CountingSort.java
+++ src/algorithm/sorting/linear/CountingSort.java
@@ -14,13 +14,21 @@ public class CountingSort {
         System.out.println(java.util.Arrays.toString(b));
     }
 
+    /**
+     * Simple sort of numbers in array;
+     * <p>The elements value shouldn't exceed {@code value a.length - 1}
+     * (e.g. array {@code {1,2,3}} is not correct, because {@code 3 > 2}</p>
+     *
+     * @param a array
+     * @return new sorted array
+     */
     public static int[] sort(int[] a) {
         int n = a.length;
         int[] cnt = new int[n];
         int[] res = new int[n];
 
-        for (int i = 0; i < n; i++) {
-            cnt[a[i]]++;
+        for (int num : a) {
+            cnt[num]++;
         }
         for (int i = 1; i < n; i++) {
             cnt[i] += cnt[i - 1];
@@ -29,6 +37,37 @@ public class CountingSort {
             int pos = cnt[a[i]] - 1;
             res[pos] = a[i];
             cnt[a[i]]--;
+        }
+        return res;
+    }
+
+    /**
+     * Sort an array of numbers by {@code exp}-digit (starting from the least significant digit
+     * ({@code 0})) in ascending order.
+     * <p>Actively used as subroutine in radix-sort.</p>
+     *
+     * @param a   original array
+     * @param exp by which number the counting is processed (starting from {@code 0})
+     * @return new sorted array
+     */
+    public static int[] sort(int[] a, int exp) {
+        final int base = 10;
+        int n = a.length;
+        int[] cnt = new int[base]; // 10 is the base of digits-alphabet
+        int[] res = new int[n];
+
+        for (int num : a) {
+            int digit = (num / exp) % base;
+            cnt[digit]++;
+        }
+        for (int i = 1; i < n; i++) {
+            cnt[i] += cnt[i - 1];
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            int digit = (a[i] / exp) % base;
+            int pos = cnt[digit] - 1;
+            res[pos] = a[i];
+            cnt[digit]--;
         }
         return res;
     }
---
+++ src/algorithm/sorting/linear/RadixSort.java
@@ -0,0 +1,45 @@
+package algorithm.sorting.linear;
+
+import java.util.Arrays;
+
+/**
+ * @author BaladKV
+ * @since 13.06.2020
+ */
+public class RadixSort {
+
+    public static void main(String[] args) {
+        int[] a = new int[]{1, 9, 2, 5, 8, 3, 7, 4, 6, 5};
+        int[] b = sort(a);
+        System.out.println(java.util.Arrays.toString(b));
+    }
+
+    /**
+     * Sort numbers in ascending order.
+     * <p>As a sub-routine uses CountingSort.</p>
+     *
+     * @param a array with nums
+     * @return new (sorted) array
+     */
+    public static int[] sort(int[] a) {
+        int max = getMaxValue(a);
+        int[] newA = Arrays.copyOf(a, a.length);
+        for (int exp = 1; max / exp > 0; exp *= 10) {
+            newA = CountingSort.sort(newA, exp);
+        }
+        return newA;
+    }
+
+    private static int getMaxValue(int[] a) {
+        if (a == null || a.length == 0) {
+            throw new IllegalArgumentException("Array should not be empty");
+        }
+        int max = a[0];
+        for (int i = 1; i < a.length; i++) {
+            if (a[i] > max) {
+                max = a[i];
+            }
+        }
+        return max;
+    }
+}
